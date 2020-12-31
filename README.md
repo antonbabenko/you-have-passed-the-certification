33b90ec6c3bb9d8326b6f36474a4ee07c71ad0ab
Anton Babenko <anton@antonbabenko.com>
2020-12-31 13:04:09 +0100

    Added all together

---
+++ README.md
@@ -0,0 +1,20 @@
+# Chrome Extension - You have passed the certification!
+
+I just don't want to see posts about it in LinkedIn newsfeed.
+
+See `content.js` for predefined list of items to hide in the newsfeed.
+
+
+## How to install?
+
+1. Clone this repository locally or [download repository as ZIP](https://github.com/antonbabenko/you-have-passed-the-certification/archive/master.zip) and unzip it locally.
+2. In Chrome, at the top right, click `More`, then `More tools`, and then `Extensions`.
+3. At the top left, click `Upload unpacked extension` and select directory where you unzipped it.
+4. Enjoy your updated [Linkedin newsfeed](https://www.linkedin.com/feed/).
+
+
+## Potential todos:
+
+* Consider hiding also images by image sizes:
+  - 800x565 - AWS DevOps pro
+  - 800x617 - AWS SA pro
---
+++ content.js
@@ -0,0 +1,60 @@
+function findAndHide(element) {
+  let items = new Array;
+
+  let qs = [
+    // By URLs
+    "a[href*='youracclaim.com/badges/']",
+    "a[href*='www.credential.net/']",
+    "a[href*='bcert.me/']",
+    "a[href*='university.mongodb.com/course_completion/']",
+
+    // Everything with tags
+    "a[href*='linkedin.com/feed/hashtag/?keywords=awscertified']",
+    "a[href*='linkedin.com/feed/hashtag/?keywords=azurecertified']",
+    "a[href*='linkedin.com/feed/hashtag/?keywords=microsoftcertified']",
+
+    // Certificates
+    "img[alt='AWS Certified Developer-Associate']"
+  ];
+
+  qs.forEach(function (qs) {
+    items.push(...element.querySelectorAll(qs));
+  });
+
+  // console.log("Found these items:", items);
+
+  items.forEach(function (item) {
+    let current = item;
+
+    while (current && current.parentNode) {
+      if (current.hasAttribute("data-id")) {
+        // console.log("Hiding element: ", current);
+        current.classList.add("visually-hidden")
+        // current.style.border = "10px solid red"
+      }
+      current = current.parentNode;
+    }
+
+  });
+}
+
+// Select the node that will be observed for mutations
+const targetNode = document.getElementById('voyager-feed');
+
+// Options for the observer (which mutations to observe)
+const config = { childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+  for(const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      findAndHide(mutation.target)
+    }
+  }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
